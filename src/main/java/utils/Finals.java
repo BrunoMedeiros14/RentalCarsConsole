303256650029e03ecb52bedce2e6525c0625e9fa
Bruno <brunodemedeiros14@gmail.com>
2023-05-17 00:15:18 -0300

    New colors on class Finals.

--- src/main/java/utils/Finals.java
+++ src/main/java/utils/Finals.java
@@ -8,9 +8,6 @@ public class Finals {
 
   public static final String CLEAN = "\033[H\033[2J";
 
-  public static final String RED = "\u001B[31m";
-  public static final String GREEN = "\u001B[32m";
-  public static final String BLUE = "\u001B[36m";
   public static final String RESET = "\u001B[0m";
   public static final String HOME_MENU =
     """
@@ -33,4 +30,24 @@ public class Finals {
     1 - ID
     2 - Get all
     0 - Return""";
+
+  // Regular Colors
+  public static final String BLACK = "\033[0;30m"; // BLACK
+  public static final String RED = "\033[0;31m"; // RED
+  public static final String GREEN = "\033[0;32m"; // GREEN
+  public static final String YELLOW = "\033[0;33m"; // YELLOW
+  public static final String BLUE = "\033[0;34m"; // BLUE
+  public static final String PURPLE = "\033[0;35m"; // PURPLE
+  public static final String CYAN = "\033[0;36m"; // CYAN
+  public static final String WHITE = "\033[0;37m"; // WHITE
+
+  // Bold
+  public static final String BLACK_BOLD = "\033[1;30m"; // BLACK
+  public static final String RED_BOLD = "\033[1;31m"; // RED
+  public static final String GREEN_BOLD = "\033[1;32m"; // GREEN
+  public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
+  public static final String BLUE_BOLD = "\033[1;34m"; // BLUE
+  public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
+  public static final String CYAN_BOLD = "\033[1;36m"; // CYAN
+  public static final String WHITE_BOLD = "\033[1;37m"; // WHITE
 }
