d46180dada1ea4fe92c81d79f4fe41b01a8f5f4c
Bruno <brunodemedeiros14@gmail.com>
2023-05-17 00:16:08 -0300

    Delete comment.

--- src/main/java/utils/SqlConnection.java
+++ src/main/java/utils/SqlConnection.java
@@ -1,29 +1,12 @@
 package utils;
 
-// import java.sql.Connection;
-// import java.sql.DriverManager;
-
-// public class SqlConnection {
-//   private static String user = "root";
-//   private static String password = "asdf";
-//   private static String url = "jdbc:sqlserver://localhost:3306/rental-cars";
-
-//   private static Connection conn;
-
-//   public static Connection getConnection() {
-
-//     return conn;
-//   }
-
-// }
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SqlConnection implements AutoCloseable {
 
-  private final boolean LITE = true;
+  private final boolean LITE = false;
   private final String URL = "jdbc:mysql://localhost:3306/rental-cars";
   private final String URL_LITE = "jdbc:sqlite:./database.db";
   private final String USERNAME = "root";
